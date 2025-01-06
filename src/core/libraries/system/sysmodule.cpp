190c71ff9536819e9824f40db28048479712ecc9
ChronoRPG <106481523+ChronoRPG@users.noreply.github.com>
2025-01-06 01:17:09 +0000

    Update sysmodule.cpp  La brujita

--- src/core/libraries/system/sysmodule.cpp
+++ src/core/libraries/system/sysmodule.cpp
@@ -38,6 +38,13 @@ int PS4_SYSV_ABI sceSysmoduleIsLoaded(OrbisSysModule id) {
     if (static_cast<u16>(id) == 0) {
         LOG_ERROR(Lib_SysModule, "Invalid sysmodule ID: {:#x}", static_cast<u16>(id));
         return ORBIS_SYSMODULE_INVALID_ID;
+
+           }
+
+    // Simulación específica para ORBIS_SYSMODULE_SAVE_DATA_DIALOG
+    if (id == ORBIS_SYSMODULE_SAVE_DATA_DIALOG) {
+        LOG_INFO(Lib_SysModule, "Simulating ORBIS_SYSMODULE_SAVE_DATA_DIALOG as loaded.");
+        return ORBIS_OK; // Indicar que el módulo está "cargado"
     }
     return ORBIS_OK;
 }
